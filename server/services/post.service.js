62b7bdf52f2d568c5a6fd2eb401398e79fd07790
moshiour rahman <moshiour0027@gmail.com>
2020-03-29 16:55:05 +0600

    update

--- server/services/post.service.js
+++ server/services/post.service.js
@@ -1,42 +1,42 @@
 var Post = require("../models/post");
 
-exports.getPosts = async function() {
-  var query = {
-    limit: 10,
-    order: [["createdAt", "DESC"]]
-  };
-  try {
-    var posts = await Post.findAll(query);
+exports.getPosts = async function () {
+    var query = {
+        limit: 10,
+        order: [["createdAt", "DESC"]]
+    };
+    try {
+        var posts = await Post.findAll(query);
 
-    return posts;
-  } catch (e) {
-    throw Error("Error while getting Posts");
-  }
+        return posts;
+    } catch (e) {
+        throw Error("Error while getting Posts");
+    }
 };
 
-exports.createPost = async function(entity) {
-  try {
-    var result = await Post.create(entity);
-    return result;
-  } catch {
-    if (err) {
-      throw Error("Error when creating post");
+exports.createPost = async function (entity) {
+    try {
+        var result = await Post.create(entity);
+        return result;
+    } catch (error) {
+        if (error) {
+            throw Error("Error when creating post");
+        }
     }
-  }
 };
 
-exports.readPost = async function(id) {
-  var query = {
-    where: {
-      post_id: id
+exports.readPost = async function (id) {
+    var query = {
+        where: {
+            post_id: id
+        }
+    };
+    try {
+        var post = await Post.findOne(query);
+        return post;
+    } catch (err) {
+        if (err) {
+            throw Error("Error when creating post");
+        }
     }
-  };
-  try {
-    var post = await Post.findOne(query);
-    return post;
-  } catch {
-    if (err) {
-      throw Error("Error when creating post");
-    }
-  }
 };
